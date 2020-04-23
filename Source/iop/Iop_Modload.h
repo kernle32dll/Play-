7ef7b858e42a95deebcddc0a7df4c8af2e87f3e1
Björn Gerdau <bjoerngerdau@gmx.de>
2020-04-23 18:43:50 +0200

    Implement CModload::GetModuleIdListByName
    Used by Burnout 3 to query for lgAud and USB_driver

--- Source/iop/Iop_Modload.h
+++ Source/iop/Iop_Modload.h
@@ -25,6 +25,7 @@ namespace Iop
 		uint32 GetModuleIdList(uint32, uint32, uint32);
 		uint32 StopModule(uint32, uint32, uint32, uint32);
 		int32 ReferModuleStatus(uint32, uint32);
+		int32 GetModuleIdListByName(uint32, uint32, uint32, uint32);
 		int32 SearchModuleByName(uint32);
 		int32 AllocLoadMemory(uint32, uint32, uint32);
 
