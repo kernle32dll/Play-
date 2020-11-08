c7418af5a6804b2874217bee5c30fb3df259cf7c
Björn Gerdau <bjoerngerdau@gmx.de>
2020-11-08 23:09:36 +0100

    Fix missing systemcall 0x6B

--- Source/ee/PS2OS.h
+++ Source/ee/PS2OS.h
@@ -371,6 +371,7 @@ private:
 	void sc_ReferSemaStatus();
 	void sc_GetOsdConfigParam();
 	void sc_FlushCache();
+	void sc_SifStopDma();
 	void sc_GsGetIMR();
 	void sc_GsPutIMR();
 	void sc_SetVSyncFlag();
