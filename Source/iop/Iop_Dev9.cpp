09d2f7190dc078d1cd63d73644a6117600419420
Björn Gerdau <bjoerngerdau@gmx.de>
2021-05-17 16:05:17 +0200

    WIP IOP Dev9 improvements

--- Source/iop/Iop_Dev9.cpp
+++ Source/iop/Iop_Dev9.cpp
@@ -10,26 +10,73 @@ uint32 CDev9::ReadRegister(uint32 address)
 	uint32 result = 0;
 	switch(address)
 	{
+	case REG_POWER:
+		result = m_Power;
+		break;
+	case REG_1460:
+		result = m_reg1460;
+		break;
+	case REG_1462:
+		result = m_reg1462;
+		break;
+	case REG_1464:
+		result = m_reg1464;
+		break;
+	case REG_1466:
+		result = m_reg1466;
+		break;
 	case REG_REV:
 		//Report expansion bay
 		result = 0x30;
 		break;
 	}
-	LogRead(address);
+	LogRead(address, result);
 	return result;
 }
 
 void CDev9::WriteRegister(uint32 address, uint32 value)
 {
+	switch(address)
+	{
+	case REG_1460:
+		 m_reg1460 = value;
+		break;
+	case REG_1462:
+		 m_reg1462 = value;
+		break;
+	case REG_1464:
+		 m_reg1464 = value;
+		break;
+	case REG_1466:
+		 m_reg1466 = value;
+		break;
+	case REG_POWER:
+		m_Power = value;
+	}
 	LogWrite(address, value);
 }
 
-void CDev9::LogRead(uint32 address)
+void CDev9::LogRead(uint32 address, uint32 result)
 {
 	switch(address)
 	{
+	case REG_1460:
+		CLog::GetInstance().Print(LOG_NAME, "= REG_1460 (0x%08X)\r\n", result);
+		break;
+	case REG_1462:
+		CLog::GetInstance().Print(LOG_NAME, "= REG_1462 (0x%08X)\r\n", result);
+		break;
+	case REG_1464:
+		CLog::GetInstance().Print(LOG_NAME, "= REG_1464 (0x%08X)\r\n", result);
+		break;
+	case REG_1466:
+		CLog::GetInstance().Print(LOG_NAME, "= REG_1466 (0x%08X)\r\n", result);
+		break;
+	case REG_POWER:
+		CLog::GetInstance().Print(LOG_NAME, "= REG_POWER (0x%08X)\r\n", result);
+		break;
 	case REG_REV:
-		CLog::GetInstance().Print(LOG_NAME, "= REG_REV\r\n");
+		CLog::GetInstance().Print(LOG_NAME, "= REG_REV (0x%08X)\r\n", result);
 		break;
 	default:
 		CLog::GetInstance().Warn(LOG_NAME, "Read an unknown register 0x%08X.\r\n", address);
@@ -41,6 +88,21 @@ void CDev9::LogWrite(uint32 address, uint32 value)
 {
 	switch(address)
 	{
+	case REG_1460:
+		CLog::GetInstance().Print(LOG_NAME, "REG_1460 = 0x%08X\r\n", value);
+		break;
+	case REG_1462:
+		CLog::GetInstance().Print(LOG_NAME, "REG_1462 = 0x%08X\r\n", value);
+		break;
+	case REG_1464:
+		CLog::GetInstance().Print(LOG_NAME, "REG_1464 = 0x%08X\r\n", value);
+		break;
+	case REG_1466:
+		CLog::GetInstance().Print(LOG_NAME, "REG_1466 = 0x%08X\r\n", value);
+		break;
+	case REG_POWER:
+		CLog::GetInstance().Print(LOG_NAME, "REG_POWER = 0x%08X\r\n", value);
+		break;
 	default:
 		CLog::GetInstance().Warn(LOG_NAME, "Wrote 0x%08X to an unknown register 0x%08X.\r\n", value, address);
 		break;
--- Source/iop/Iop_Dev9.h
+++ Source/iop/Iop_Dev9.h
@@ -19,10 +19,23 @@ namespace Iop
 	private:
 		enum
 		{
+			// These have no documented name
+			REG_1460 = 0x1F801460,
+			REG_1462 = 0x1F801462,
+			REG_1464 = 0x1F801464,
+			REG_1466 = 0x1F801466,
+
+			REG_POWER = 0x1F80146C,
 			REG_REV = 0x1F80146E,
 		};
 
-		void LogRead(uint32);
+		void LogRead(uint32, uint32);
 		void LogWrite(uint32, uint32);
+
+		uint32 m_reg1460;
+		uint32 m_reg1462;
+		uint32 m_reg1464;
+		uint32 m_reg1466;
+		uint32 m_Power;
 	};
 }
