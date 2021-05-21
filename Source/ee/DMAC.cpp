538b0deda3724da215ad74e611e682440a0e40bc
Björn Gerdau <bjoerngerdau@gmx.de>
2021-05-21 20:09:52 +0200

    Add alternative D_STAT

--- Source/ee/DMAC.cpp
+++ Source/ee/DMAC.cpp
@@ -917,6 +917,7 @@ void CDMAC::SetRegister(uint32 nAddress, uint32 nData)
 		break;
 
 	case D_STAT + 0x0:
+	case D_STAT_ALT + 0x0:
 	{
 		uint32 nStat = nData & 0x0000FFFF;
 		uint32 nMask = nData & 0xFFFF0000;
@@ -933,6 +934,9 @@ void CDMAC::SetRegister(uint32 nAddress, uint32 nData)
 	case D_STAT + 0x4:
 	case D_STAT + 0x8:
 	case D_STAT + 0xC:
+	case D_STAT_ALT + 0x4:
+	case D_STAT_ALT + 0x8:
+	case D_STAT_ALT + 0xC:
 		break;
 
 	case D_PCR + 0x0:
